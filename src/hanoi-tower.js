e51f39d557cc34baad2b35d36f6d4d927c6df00a
Green1ce <cha1iiep@mail.ru>
2021-03-07 23:10:57 +0300

    feat: add function for hanoi-tower and transform-array

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  let obj = new Object();
+  let answ1, answ2;
+  answ1 = Math.pow(2, disksNumber) - 1;
+  answ2 = (1 * answ1)/(turnsSpeed / 3600);
+  obj.turns = answ1;
+  obj.seconds = Math.floor(answ2);
+  return obj;
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let answer = [];
+  if (typeof arr != 'object') return 'THROWN';
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == '--discard-next' || arr[i] == '--discard-prev' || arr[i] == '--double-next' || arr[i] == '--double-prev') continue;
+    else if (arr[i - 1] == '--discard-next') continue;
+    else if (arr[i - 1] == '--double-next') {
+      if (arr[i + 1] == '--double-prev') answer.push(arr[i], arr[i], arr[i]);
+      else if (arr[i + 1] == '--discard-prev') answer.push(arr[i]);
+      else answer.push(arr[i], arr[i]);
+    } else if (arr[i + 1] == '--discard-prev') continue;
+    else if (arr[i + 1] == '--double-prev') answer.push(arr[i], arr[i]);
+    else answer.push(arr[i]);
+  }
+  return answer;
 };
