3c4ecdb59abf2b9a6755bb6a97a425d4210fb753
Green1ce <cha1iiep@mail.ru>
2021-03-07 13:35:25 +0300

    feat: add function for carbon-dating and dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let answer = [];
+  if (typeof members != 'object' || members == null) return false;
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] == 'string') {
+      letter = (/[A-Za-z]{1}/i).exec(members[i]);
+      answer.push(letter[0].toUpperCase())
+    }
+  }
+  answer.sort();
+  return answer.join('').toUpperCase();
 };
